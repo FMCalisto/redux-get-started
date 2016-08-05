16a49b911af12fba241f2cbe8f2bf2ca3640780f
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-05 14:49:40 +0100

    [ADD] Undefined State

--- course/counter_reducer.js
+++ course/counter_reducer.js
@@ -7,6 +7,12 @@
 
 function counter(state, saction) {
 
+    if(typeof state === 'undefined') {
+
+        return 0;
+
+    }
+
     if (action.type === 'INCREMENT') {
 
         return state + 1;
@@ -48,5 +54,11 @@ expect(
     counter(1, {type: 'SOMETHING_ELSE'})
 
 ).toEqual(1);
+
+expect(
+
+    counter(undefined, {})
+
+).toEqual(0);
 
 console.log('Tests passed!');
