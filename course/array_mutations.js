09f97d57accc6692da5a35ede6b971d3b95c7113
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-05 18:54:26 +0100

    [FIXME] Test Remove Counter

--- course/array_mutations.js
+++ course/array_mutations.js
@@ -1,6 +1,23 @@
+/*
+ * Avoiding Array Mutations.
+ *
+ * @author: Francisco Maria Calisto
+ *
+ */
+
 const addCounter = (list) => {
 
+    // Concat Method does not modify the original array.
+    myList = list.concat([0]);
 
+    return myList;
+
+};
+
+const removeCounter = (list, index) => {
+
+    list.splice(index, 1);
+    return list;
 
 };
 
@@ -17,5 +34,22 @@ const testAddCounter = () => {
 
 };
 
+const testRemoveCounter = () => {
+
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+
+    //FIXME
+    deepFreeze(listBefore);
+
+    expect(
+
+        removeCounter(listBefore, 1)
+
+    ).toEqual(listAfter);
+
+};
+
 testAddCounter();
+testRemoveCounter();
 console.log('All tests passed.');
