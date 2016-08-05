30d974e8aa17e13b4fbb1e66806ea8409647ff5e
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-05 14:16:54 +0100

    [CREATE] Course Source

---
+++ course/impure.js
@@ -0,0 +1,26 @@
+
+/*
+ * Impure functions.
+ *
+ * @author: Francisco Maria Calisto
+ *
+ */
+
+// Impure functions
+function square2(x) {
+  
+  updateXInDatabase(x);
+  
+  return x * x;
+  
+}
+
+function squareAll2(items) {
+  
+  for (let i = 0; i < items.length; i++) {
+    
+    items[i] = square(items[i]);
+    
+  }
+  
+}
---
+++ course/pure.js
@@ -0,0 +1,20 @@
+
+/*
+ * Pure functions.
+ *
+ * @author: Francisco Maria Calisto
+ *
+ */
+
+// Pure functions
+function square1(x) {
+  
+   return x * x;
+  
+}
+
+function squareAll1(items) {
+  
+   return items.map(square);
+  
+}
