c7eecb7aa48da958a2ac1139912371ba4cd3d137
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-06 12:45:47 +0100

    [UPDATE] Switch Case

--- course/todo_list_reducer.js
+++ course/todo_list_reducer.js
@@ -7,7 +7,19 @@
 
 const todos = (state = [], action) => {
 
-
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        default:
+            return state;
+    }
 
 };
 
