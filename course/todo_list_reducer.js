a967a4e220cbf4961e5074580957696a0d02a404
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-06 14:42:04 +0100

    [CREATE] TODO Function

--- course/todo_list_reducer.js
+++ course/todo_list_reducer.js
@@ -5,6 +5,38 @@
  *
  */
 
+ const todo = (state, action) => {
+
+    switch (action.type) {
+
+        case 'ADD_TODO':
+            return {
+
+                id: action.id,
+                text: action.text,
+                completed: false
+
+            };
+
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+
+            return {
+
+                ...state,
+                    completed: !state.completed
+
+            };
+
+        default:
+            return state;
+
+    };
+
+ };
+
 const todos = (state = [], action) => {
 
     switch (action.type) {
@@ -13,29 +45,12 @@ const todos = (state = [], action) => {
             return [
                 
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                todo(undefined, action)
 
             ];
 
         case 'TOGGLE_TODO':
-            return state.map(todo => {
-                
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-
-                return {
-
-                    ...todo,
-                        completed: !todo.completed
-
-                };
-
-            });
+            return state.map(t => todo(t, action));
 
         default:
             return state;
