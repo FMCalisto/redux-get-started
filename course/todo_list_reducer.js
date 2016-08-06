8004da9816468d04a9bd5921e41ad059ba5bd925
FMCalisto <francisco_mcalisto@hotmail.com>
2016-08-06 13:11:14 +0100

    [CREATE] Toggle TODO

--- course/todo_list_reducer.js
+++ course/todo_list_reducer.js
@@ -8,15 +8,35 @@
 const todos = (state = [], action) => {
 
     switch (action.type) {
+
         case 'ADD_TODO':
             return [
+                
                 ...state,
                 {
                     id: action.id,
                     text: action.text,
                     completed: false
                 }
+
             ];
+
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+
+                return {
+
+                    ...todo,
+                        completed: !todo.completed
+
+                };
+
+            });
+
         default:
             return state;
     }
@@ -56,5 +76,58 @@ const testAddTodo = () => {
 
 };
 
+const testToggleTodo = () => {
+    
+    const stateBefore = [
+
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: false
+        },
+
+    ];
+
+    const action = {
+
+        type: 'TOGGLE_TODO',
+        id: 1
+
+    };
+
+    const stateAfter = [
+
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: true
+        },
+
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+
+        todos(stateBefore, action)
+
+    ).toEqual(stateAfter);
+
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed.');
