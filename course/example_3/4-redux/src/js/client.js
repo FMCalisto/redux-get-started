4b7a2fcecf13eb5e49da9a71cbb39dc101d2cf57
FMCalisto <FMCalisto@users.noreply.github.com>
2017-08-02 21:09:41 +0100

    [UPDATE] Client Redux Middleware Final

--- course/example_3/4-redux/src/js/client.js
+++ course/example_3/4-redux/src/js/client.js
@@ -1,10 +1,36 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 const reducer = (initialState = 0, action) => {
-
+  if (action.type === "INC") {
+    return initialState + 1;
+  } else if (action.type === "DEC") {
+    return initialState - 1;
+  } else if (action.type === "ERROR") {
+    throw new Error("Error!");
+  }
+  return initialState;
 }
 
-const store = createStore(reducer, 1);
+const logger = (store) => (next) => (action) => {
+  console.log("action fired", action);
+  next(action);
+}
+
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch(e) {
+    console.log("Problem...", e);
+  }
+}
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(reducer, 1, middleware);
+
+store.subscribe(() => {
+  console.log("store change", store.getState())
+})
 
 /* The action have to have a "type" into it */
 
@@ -30,4 +56,8 @@ store.dispatch({
 
 store.dispatch({
   type: "DEC"
+});
+
+store.dispatch({
+  type: "ERROR"
 });
